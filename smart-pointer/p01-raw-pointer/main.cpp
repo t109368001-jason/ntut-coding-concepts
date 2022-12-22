63663e6db5d068586257890f095a2e0968380a46
jason-xiao <t105360046@ntut.org.tw>
2022-12-22 14:33:48 +0800

    <update> update p01-raw-pointer

--- smart-pointer/p01-raw-pointer/main.cpp
+++ smart-pointer/p01-raw-pointer/main.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 
-void f1(int* data);
-void f2(int* data);
-void f3(int* data);
+typedef struct Data {
+  int data1;
+  int data2;
+  int data3;
+  ~Data() {
+    std::cout << "~Data()" << std::endl;
+  }
+} Data;
+
+void f1(Data* data);
+void f2(Data* data);
+void f3(Data* data);
 
 int main() {
-  int* data = new int;
+  Data* data = new Data;
 
   f1(data);
   f2(data);
   f3(data);
 
-  std::cout << "data=" << *data << std::endl;
+  std::cout << "data=Data(";
+  std::cout << "data1=" << data->data1 << ", ";
+  std::cout << "data2=" << data->data2 << ", ";
+  std::cout << "data3=" << data->data3;
+  std::cout << ")" << std::endl;
 
   delete data;
 
   return 0;
 }
 
-void f1(int* data) {
-  *data = 1;
+void f1(Data* data) {
+  data->data1 = 1;
 }
 
-void f2(int* data) {
-  *data = 2;
+void f2(Data* data) {
+  data->data2 = 2;
 }
 
-void f3(int* data) {
-  *data = 3;
+void f3(Data* data) {
+  data->data3 = 3;
 }
